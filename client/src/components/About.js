d8e866e9344bb7ee2de45d0cab911ecafd068e7a
Chaitya <chaitya.dobariya@spit.ac.in>
2024-05-23 08:47:55 +0530

    minor updates

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './About.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle, faTools, faLightbulb, faHandshake, faUsers } from '@fortawesome/free-solid-svg-icons';
+
 const About = () => {
   return (
     <>
@@ -9,6 +12,7 @@ const About = () => {
       <div className='circle'></div> {/* Circle as fixed background */}
       <div className='rectangle'></div>
       <div className='Project_Description'>
+      <FontAwesomeIcon icon={faInfoCircle} className='icon' />
         <p className='AboutUs_text'>About Us</p>
         <p className='AboutUs_para'>
           Our project utilizes machine learning algorithms to predict
@@ -19,6 +23,7 @@ const About = () => {
           accurate predictions based on historical data.</p>
       </div>
       <div className='Methodology'>
+      <FontAwesomeIcon icon={faTools} className='icon' />
         <p className='AboutUs_text2'>What We Did?</p>
         <p className='AboutUs_para2'>
           We have developed a predictive model that analyzes past
@@ -33,6 +38,7 @@ const About = () => {
         </p>
       </div>
       <div className='Future_Plans'>
+      <FontAwesomeIcon icon={faLightbulb} className='icon' />
         <p className='AboutUs_text3'>Future Plans</p>
         <p className='AboutUs_para3'>
           We are committed to continuously improving our predictive model by
@@ -44,6 +50,7 @@ const About = () => {
       </div>
 
       <div className='Acknowledgments'>
+      <FontAwesomeIcon icon={faHandshake} className='icon' />
         <p className='AboutUs_text4'>Acknowledgments</p>
         <p className='AboutUs_para4'>
           We would like to express our gratitude to the Mht-CET authorities
@@ -54,12 +61,13 @@ const About = () => {
 
       </div>
       <div className='Team_Members'>
+      <FontAwesomeIcon icon={faUsers} className='icon' />
         <p className='AboutUs_text5'>Team Members</p>
         <p className='AboutUs_para5'>
-          <p>Prashil Kadam</p>
-          <p>Tejas Dhurgude</p>
           <p>Sushant Divekar</p>
+          <p>Tejas Dhurgude</p>
           <p>Chaitya Dobariya</p>
+          <p>Prashil Kadam</p>
         </p>
       </div>
 
